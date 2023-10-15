e93e48fff1492cafdf70ee2bff625e4cc5755fb7
baliutkarsh2 <baliutkarsh2@gmail.com>
2023-10-15 03:26:53 -0400

    commit-5

--- lib/api_constants.dart
+++ lib/api_constants.dart
@@ -2,7 +2,7 @@
 const endpoint = 'https://api.openai.com/v1/';
 
 /// API key
-const aiToken = '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';
+const aiToken = 'openAI API key here';
 
 /// Email for Gravatar image
 const emailAddress = 'baliutkarsh2@gmail.com';
